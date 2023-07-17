a9c131f39d35859ee9f0d27fa66a8112b159f2e1
IrisLei2808 <duclt2808@gmail.com>
2023-07-17 16:01:47 -0700

    Initial Commit

---
+++ ClonePaypalScreen/ClonePaypalScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClonePaypalScreenApp.swift
+//  ClonePaypalScreen
+//
+//  Created by Duc Le on 7/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ClonePaypalScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
